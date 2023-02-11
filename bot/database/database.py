30ba5bc9f2ff4ada6b745c1f2d62c9493741cd40
Amit Jakhar <122900262+HelloUltra44@users.noreply.github.com>
2023-02-11 09:48:09 +0530

    new

--- bot/database/database.py
+++ bot/database/database.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+# (c) 
 
 import datetime
 import motor.motor_asyncio
--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K / Akshay C / @AbirHasan2005
+# (c) Shrimadhav U K / Akshay C / 
 
 # the logging things
 
