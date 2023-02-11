60be71d0948cdf6bfcfd2989288b7983594fbe89
Amit Jakhar <122900262+HelloUltra44@users.noreply.github.com>
2023-02-11 09:44:54 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,17 +6,17 @@ class Config(object):
     # You can keep this default
     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
     # Put MongoDB URL
-    DATABASE_URL = get_config("DATABASE_URL", "mongodb+srv://amit:amit@cluster0.p3gnlja.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = get_config("DATABASE_URL", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "5326623467:AAFgXRf0aZdDSmZaJFwbTKmf1uL8lAdTync")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
     # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 23560088))
-    API_HASH = get_config("API_HASH", "999c01704d5c417749a98f4b8785fe88")
-    LOG_CHANNEL = get_config("LOG_CHANNEL", "comprssorlogs")
+    APP_ID = int(get_config("APP_ID", ))
+    API_HASH = get_config("API_HASH", "")
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", "anumitultrabots") # Without `@` LOL
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in get_config("AUTH_USERS","5410723702"))
+    AUTH_USERS = set(int(x) for x in get_config("AUTH_USERS",""))
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
